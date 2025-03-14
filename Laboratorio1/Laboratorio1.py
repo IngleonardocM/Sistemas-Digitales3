933eeaec10be9611e2f80cc43be0c193d6e4c78c
IngleonardocM <davidle.castanoma@ecci.edu.co>
2025-03-13 23:35:55 -0500

    Documentación

--- Laboratorio1/Laboratorio1.py
+++ Laboratorio1/Laboratorio1.py
@@ -1,79 +1,70 @@
 
+def mostrar_menu():
+    print("\n=== Menú del Directorio Telefónico ===")
+    print("1. Agregar un nuevo contacto")
+    print("2. Buscar un contacto por teléfono")
+    print("3. Eliminar un contacto")
+    print("4. Salir")
 
-# Definimos la clase Contacto
-class Contacto:
-    def __init__(self, nombre, telefono, cumpleanos, correo):
-        self.nombre = nombre
-        self.telefono = telefono
-        self.cumpleanos = cumpleanos
-        self.correo = correo
 
-    def mostrar_info(self):
-        return f"Nombre: {self.nombre}, Teléfono: {self.telefono}, Cumpleaños: {self.cumpleanos}, Correo: {self.correo}"
-
-# Lista para almacenar la base de datos de contactos
-directorio = []
-
-# Lista de textos a mostrar durante la ejecución del programa
-mensajes = [
-    "Bienvenido al directorio telefónico",
-    "Digite el Nombre y Apellido: ",
-    "Digite el Teléfono Celular: ",
-    "Digite el Cumpleaños (DD/MM/AAAA): ",
-    "Digite el Correo Electrónico: ",
-    "1. Agregar un nuevo registro",
-    "2. Buscar una persona por teléfono celular",
-    "3. Borrar un registro",
-    "4. Salir de la aplicación",
-    "Opción no válida. Intente de nuevo.",
-    "Gracias por usar el directorio telefónico"
-]
-
-def agregar_contacto():
-    nombre = input(mensajes[1])
-    telefono = input(mensajes[2])
-    cumpleanos = input(mensajes[3])
-    correo = input(mensajes[4])
-    nuevo_contacto = Contacto(nombre, telefono, cumpleanos, correo)
-    directorio.append(nuevo_contacto)
+def agregar_registro(base_datos):
+    print("\n--- Agregar un nuevo contacto ---")
+    persona = {}
+    persona["Nombre y Apellido"] = input("Ingrese el nombre y apellido: ")
+    persona["Teléfono celular"] = input("Ingrese el número de teléfono: ")
+    persona["Cumpleaños"] = input("Ingrese la fecha de cumpleaños: ")
+    persona["Correo"] = input("Ingrese el correo electrónico: ")
+    base_datos.append(persona)
     print("Contacto agregado exitosamente.\n")
 
-def buscar_contacto():
-    telefono = input("Ingrese el número de teléfono a buscar: ")
-    for contacto in directorio:
-        if contacto.telefono == telefono:
-            print("Contacto encontrado:", contacto.mostrar_info())
-            return
-    print("Contacto no encontrado.\n")
 
-def borrar_contacto():
-    telefono = input("Ingrese el número de teléfono a borrar: ")
-    for contacto in directorio:
-        if contacto.telefono == telefono:
-            directorio.remove(contacto)
+def buscar_por_telefono(base_datos):
+    print("\n--- Buscar un contacto ---")
+    telefono = input("Ingrese el número de teléfono a buscar: ")
+    encontrado = False
+    for persona in base_datos:
+        if persona["Teléfono celular"] == telefono:
+            print("Contacto encontrado:")
+            print(f"Nombre: {persona['Nombre y Apellido']}")
+            print(f"Teléfono: {persona['Teléfono celular']}")
+            print(f"Cumpleaños: {persona['Cumpleaños']}")
+            print(f"Correo: {persona['Correo']}")
+            encontrado = True
+            break
+    if not encontrado:
+        print("No se encontró ningún contacto con ese número.\n")
+
+
+def borrar_registro(base_datos):
+    print("\n--- Eliminar un contacto ---")
+    telefono = input("Ingrese el número de teléfono del contacto a eliminar: ")
+    for persona in base_datos:
+        if persona["Teléfono celular"] == telefono:
+            base_datos.remove(persona)
             print("Contacto eliminado correctamente.\n")
             return
-    print("Contacto no encontrado.\n")
+    print("No se encontró ningún contacto con ese número.\n")
 
-def mostrar_menu():
-    print(mensajes[0])
-    while True:
-        print("\n" + "\n".join(mensajes[5:9]))
-        opcion = input("Seleccione una opción: ")
-        if opcion == "1":
-            agregar_contacto()
-        elif opcion == "2":
-            buscar_contacto()
-        elif opcion == "3":
-            borrar_contacto()
-        elif opcion == "4":
-            print(mensajes[10])
-            break
-        else:
-            print(mensajes[9])
 
-# Ejecutar el programa
-mostrar_menu()
+# Lista para almacenar registros
+base_datos = []
+
+print("¡Bienvenido al directorio telefónico!")
+while True:
+    mostrar_menu()
+    opcion = input("Seleccione una opción: ")
+    
+    if opcion == "1":
+        agregar_registro(base_datos)
+    elif opcion == "2":
+        buscar_por_telefono(base_datos)
+    elif opcion == "3":
+        borrar_registro(base_datos)
+    elif opcion == "4":
+        print("Gracias por usar el directorio. ¡Hasta pronto!")
+        break
+    else:
+        print("Opción no válida, por favor intente de nuevo.\n")
 
 
 
--- Laboratorio1/README.md
+++ Laboratorio1/README.md
@@ -2,3 +2,5 @@
 ## Integrantes
 [David Leonardo Castaño Madrigal](https://github.com/IngleonardocM) 
  
+ ## Documentación
+En el archivo [Laboratorio1.py](/Laboratorio1/Laboratorio1.py)
